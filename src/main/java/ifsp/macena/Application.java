6db04e345ec0aae5f37d5f5ff1fcf125dfc16c4b
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-09-15 02:13:30 -0300

    feat: adding exercises 4 and 5

--- src/main/java/ifsp/macena/Application.java
+++ src/main/java/ifsp/macena/Application.java
@@ -24,6 +24,46 @@ public class Application {
         int soma = SomaCalculation.calculateSoma();
         System.out.println("O valor de SOMA ao final do processamento é: " + soma);
 
+        System.out.println("Sequências:");
+        System.out.println("a) 1, 3, 5, 7, " + nextInSequenceA());
+        System.out.println("b) 2, 4, 8, 16, 32, 64, " + nextInSequenceB());
+        System.out.println("c) 0, 1, 4, 9, 16, 25, 36, " + nextInSequenceC());
+        System.out.println("d) 4, 16, 36, 64, " + nextInSequenceD());
+        System.out.println("e) 1, 1, 2, 3, 5, 8, " + nextInSequenceE());
+        System.out.println("f) 2, 10, 12, 16, 17, 18, 19, " + nextInSequenceF());
+
+        System.out.println("\nInterruptores e Lâmpadas:");
+        System.out.println("1. Ligar o primeiro interruptor e esperar alguns minutos.");
+        System.out.println("2. Desligar o primeiro interruptor e ligar o segundo interruptor.");
+        System.out.println("3. Ir até a sala das lâmpadas.");
+        System.out.println("4. A lâmpada que está acesa é controlada pelo segundo interruptor.");
+        System.out.println("5. A lâmpada que está quente mas apagada é controlada pelo primeiro interruptor.");
+        System.out.println("6. A lâmpada que está fria e apagada é controlada pelo terceiro interruptor.");
+
         scanner.close();
+    }
+
+    private static int nextInSequenceA() {
+        return 9;
+    }
+
+    private static int nextInSequenceB() {
+        return 128;
+    }
+
+    private static int nextInSequenceC() {
+        return 49;
+    }
+
+    private static int nextInSequenceD() {
+        return 100;
+    }
+
+    private static int nextInSequenceE() {
+        return 13;
+    }
+
+    private static int nextInSequenceF() {
+        return 200;
     }
 }
